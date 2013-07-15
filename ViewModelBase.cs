833d5875180915f23e6da30ee07debf16332987f
Guillermo Julián <gjulian93@gmail.com>
2013-07-15 16:54:55 +0200

    initial commit

---
+++ ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using AncoraMVVM;
+
+namespace Goinout.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged 
+    {
+        public ViewModelBase(INavigationService navigationService, INotificationService notificationService, IDispatcher dispatcher)
+        {
+        }
+    }
+}
