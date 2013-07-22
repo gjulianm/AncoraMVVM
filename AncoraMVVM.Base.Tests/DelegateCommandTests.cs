d914cc1621b0d4ef646ab0b5616f310acbb978ff
Guillermo Julián <gjulian93@gmail.com>
2013-07-22 18:03:24 +0200

    Improvements on commands

---
+++ AncoraMVVM.Base.Tests/DelegateCommandTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+
+namespace AncoraMVVM.Base.Test
+{
+    [TestClass]
+    public class DelegateCommandTests
+    {
+        [TestMethod]
+        public void CanExecute_NoConstructorParams_IsTrue()
+        {
+            var cmd = new DelegateCommand(null);
+            Assert.IsTrue(cmd.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void Execute_CalledActionWithParameter()
+        {
+            var param = new object();
+            object receivedParam = null;
+            var cmd = new DelegateCommand(p => receivedParam = p);
+
+            cmd.Execute(param);
+            Assert.AreSame(param, receivedParam);
+        }
+
+
+        private class DummyNotifier : INotifyPropertyChanged
+        {
+            private int number;
+            public int Number
+            {
+                get
+                {
+                    return number;
+                }
+                set
+                {
+                    number = value;
+                    if (PropertyChanged != null)
+                        PropertyChanged(this, new PropertyChangedEventArgs("Number"));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        [TestMethod]
+        public void BindToPropertyChange_Propagated()
+        {
+            bool wasCalled = false;
+            var not = new DummyNotifier();
+            var cmd = new DelegateCommand(null, (p) => p != null);
+            cmd.BindCanExecuteToProperty(not, "Number");
+            cmd.CanExecuteChanged += (s, e) => wasCalled = true;
+
+            not.Number = 123;
+            Assert.IsTrue(wasCalled);
+        }
+    }
+}
---
+++ AncoraMVVM.Base/NavigateCommand.cs
@@ -0,0 +1,28 @@
+﻿using AncoraMVVM.Base.Interfaces;
+using AncoraMVVM.Base.IoC;
+using System;
+using System.Windows.Input;
+
+namespace AncoraMVVM.Base
+{
+    public class NavigateCommand : ICommand
+    {
+        public string Target { get; set; }
+        public NavigateCommand(string page)
+        {
+            Target = page;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            Dependency.Resolve<INavigationService>().Navigate(Target);
+        }
+    }
+}
