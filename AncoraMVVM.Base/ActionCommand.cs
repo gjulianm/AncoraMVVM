fdf31960e75c6468edf06514fa2d8c537ee1995c
Guillermo Julián <gjulian93@gmail.com>
2013-07-22 13:52:10 +0200

    Created ActionCommand

---
+++ AncoraMVVM.Base/ActionCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace AncoraMVVM.Base
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action<object> executeAction;
+        private Func<object, bool> canExecuteAction;
+
+        public DelegateCommand(Action<object> execute)
+        {
+            executeAction = execute;
+            canExecuteAction = p => true;
+        }
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            executeAction = execute;
+            canExecuteAction = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecuteAction(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            executeAction(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, new EventArgs());
+        }
+
+        public void BindCanExecuteToProperty(INotifyPropertyChanged element, params string[] propertyNames)
+        {
+            element.PropertyChanged += (sender, e) =>
+            {
+                if (propertyNames.Contains(e.PropertyName))
+                    RaiseCanExecuteChanged();
+            };
+        }
+    }
+}
