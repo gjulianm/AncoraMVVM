ed03c049760cc0741d931e8ebb1078407a53df86
Guillermo Julián <gjulian93@gmail.com>
2013-08-17 12:41:23 +0200

    ObservableCollection extensions.

---
+++ AncoraMVVM.Base/Collections/ObservableCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AncoraMVVM.Base
+{
+    public static class IListExtension
+    {
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                list.Add(item);
+        }
+    }
+}
