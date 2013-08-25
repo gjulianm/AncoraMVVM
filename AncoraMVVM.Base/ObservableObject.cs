342b1693438cbe4d0e2f54e14b6beba9add955a8
Guillermo Julián <gjulian93@gmail.com>
2013-08-25 16:31:26 +0200

    Added ObservableObject

---
+++ AncoraMVVM.Base/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace AncoraMVVM.Base
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            var evHandler = PropertyChanged;
+            if (evHandler != null)
+                evHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
