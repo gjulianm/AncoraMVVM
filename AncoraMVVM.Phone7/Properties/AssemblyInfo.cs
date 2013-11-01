adcac7c1a139e73904216137139de53beb0de60c
Guillermo Julián <gjulian93@gmail.com>
2013-11-01 13:05:45 +0100

    Nuget package for Phone7

--- AncoraMVVM.Phone7/Properties/AssemblyInfo.cs
+++ AncoraMVVM.Phone7/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Resources;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AncoraMVVM.Phone7")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MVVM bundle for Windows Phone 7")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Guillermo Julián")]
 [assembly: AssemblyProduct("AncoraMVVM.Phone7")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("MPL  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +31,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
