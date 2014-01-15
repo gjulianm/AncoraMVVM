3151760ce086846a3e3ce143657b8a2d7f85c917
Guillermo Julián <gjulian93@gmail.com>
2014-01-15 20:59:06 +0100

    Add Testing project for testing helpers.

---
+++ AncoraMVVM.Testing/BaseMockProvider.cs
@@ -0,0 +1,51 @@
+﻿using AncoraMVVM.Base.Interfaces;
+using AncoraMVVM.Base.IoC;
+namespace AncoraMVVM.Testing
+{
+    public class BaseMockProvider : IObjectProvider
+    {
+        public static BaseMockProvider LastInstance { get; private set; }
+
+        public BaseMockProvider()
+        {
+            LastInstance = this;
+            NotificationService = GetSubstituteFor<INotificationService>();
+            Dispatcher = GetSubstituteFor<IDispatcher>();
+            Progress = GetSubstituteFor<IProgressIndicator>();
+            NavigationService = GetSubstituteFor<INavigationService>();
+            ConfigurationManager = GetSubstituteFor<IConfigurationManager>();
+            Messager = GetSubstituteFor<IMessager>();
+        }
+
+        public INotificationService NotificationService { get; set; }
+        public IDispatcher Dispatcher { get; set; }
+        public IProgressIndicator Progress { get; set; }
+        public INavigationService NavigationService { get; set; }
+        public IConfigurationManager ConfigurationManager { get; set; }
+        public IMessager Messager { get; set; }
+
+        public virtual T GetSubstituteFor<T>() where T : class
+        {
+            return null;
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            if (NotificationService is T)
+                return (T)NotificationService;
+            else if (Dispatcher is T)
+                return (T)Dispatcher;
+            else if (Progress is T)
+                return (T)Progress;
+            else if (NavigationService is T)
+                return (T)NavigationService;
+            else if (ConfigurationManager is T)
+                return (T)ConfigurationManager;
+            else if (Messager is T)
+                return (T)Messager;
+            else
+                return GetSubstituteFor<T>();
+        }
+
+    }
+}
---
+++ AncoraMVVM.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("AncoraMVVM.Testing")]
+[assembly: AssemblyDescription("Testing classes for AncoraMVVM.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Guillermo Julián")]
+[assembly: AssemblyProduct("AncoraMVVM.Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.5.*")]
+[assembly: AssemblyFileVersion("0.1.5.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: AssemblyInformationalVersion("0.1.5-alpha")]
+[assembly: InternalsVisibleTo("AncoraMVVM.Base.Tests")]
