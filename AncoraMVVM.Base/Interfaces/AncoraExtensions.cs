12e5d96924227be48086aef28bdc2d3f0a1dfcdb
Guillermo Julián <gjulian93@gmail.com>
2014-04-13 12:27:53 +0200

    Package updates

---
+++ AncoraMVVM.Base/Interfaces/AncoraExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+using AncoraMVVM.Base.IoC;
+namespace AncoraMVVM.Base.Interfaces
+{
+    public static class AncoraExtensions
+    {
+        public static void MessageAndNavigate<TViewModel, TMessage>(this INavigationService navigator, TMessage message)
+            where TViewModel : ViewModelBase
+            where TMessage : class
+        {
+            var messager = Dependency.Resolve<IMessager>();
+            messager.SendTo<TViewModel, TMessage>(message);
+            navigator.Navigate<TViewModel>();
+        }
+    }
+}
