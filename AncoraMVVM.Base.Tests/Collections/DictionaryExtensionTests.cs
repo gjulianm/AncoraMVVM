1ed3d0bea3f6d8f4a7c5c53819ece2b74a8b8dee
Guillermo Julián <gjulian93@gmail.com>
2014-09-04 20:55:04 +0200

    Added dictionary GetOrCreate method.

---
+++ AncoraMVVM.Base.Tests/Collections/DictionaryExtensionTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AncoraMVVM.Base.Tests.Collections
+{
+    [TestFixture]
+    public class DictionaryExtensionTests
+    {
+        private class DummyClass
+        {
+            public DummyClass()
+            {
+                Name = "created from DummyClass()";
+            }
+
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void GetOrCreate_ElementExists_DoesntCreateIt()
+        {
+            var dic = new Dictionary<int, DummyClass>();
+            var val = new DummyClass { Name = "asd" };
+
+            dic[0] = val;
+
+            Assert.AreSame(val, dic.GetOrCreate(0));
+            Assert.AreEqual(val.Name, dic.GetOrCreate(0).Name);
+            Assert.AreEqual(1, dic.Count);
+        }
+
+        [Test]
+        public void GetOrCreate_ElementDoesntExists_CreatesIt()
+        {
+            var dic = new Dictionary<int, DummyClass>();
+            var val = dic.GetOrCreate(0);
+
+            Assert.AreEqual(1, dic.Count);
+            Assert.IsNotNull(val);
+            Assert.AreEqual(new DummyClass().Name, val.Name);
+        }
+    }
+}
---
+++ AncoraMVVM.Base/Collections/DictionaryExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Get the element for the given key or create it with its default constructor.
+        /// </summary>
+        /// <typeparam name="TKey">Key type</typeparam>
+        /// <typeparam name="TValue">Value type</typeparam>
+        /// <param name="dic">Dictionary</param>
+        /// <param name="key">Key to retrieve</param>
+        /// <returns>The item retrieved or created</returns>
+        public static TValue GetOrCreate<TKey, TValue>(this Dictionary<TKey, TValue> dic, TKey key) where TValue : new()
+        {
+            TValue value;
+
+            if (!dic.TryGetValue(key, out value))
+            {
+                value = new TValue();
+                dic[key] = value;
+            }
+
+            return value;
+        }
+    }
+}
