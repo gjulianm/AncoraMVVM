0cb607c65323b33a33e00df9c4373562e6d3a938
Guillermo Julián <gjulianm@outlook.com>
2015-06-23 21:10:35 +0200

    Move directories

--- AncoraMVVM.Phone/Properties/AssemblyInfo.cs
+++ AncoraMVVM.Phone/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AncoraMVVM.Phone")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AncoraMVVM.Phone7")]
+[assembly: AssemblyDescription("MVVM bundle for Windows Phone 7")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AncoraMVVM.Phone")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Guillermo Julián")]
+[assembly: AssemblyProduct("AncoraMVVM.Phone7")]
+[assembly: AssemblyCopyright("MPL  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("19f11b56-f786-4c60-9a32-6f64934568fc")]
+[assembly: Guid("89b38cc5-a3bb-4c11-a21f-aeed6648736a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.29.*")]
+[assembly: AssemblyFileVersion("0.1.29.0")]
+[assembly: AssemblyInformationalVersion("0.1.29-alpha")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
