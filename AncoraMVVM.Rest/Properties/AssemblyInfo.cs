8e290117c2ee0bfd1cad9af0723fb14be42948d9
Guillermo Julián <guillermo@gjulianm.me>
2015-08-30 13:08:32 +0200

    AncoraMVVM.Rest version bump to 1.0

--- AncoraMVVM.Rest/Properties/AssemblyInfo.cs
+++ AncoraMVVM.Rest/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Resources;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*.*")]
-[assembly: AssemblyVersion("0.1.13.0")]
-[assembly: AssemblyFileVersion("0.1.13.0")]
-[assembly: AssemblyInformationalVersion("0.1.13-alpha")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
